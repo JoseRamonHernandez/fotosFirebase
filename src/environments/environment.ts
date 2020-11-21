25fb3ceb1ddf367faadc44fdfad971152f361c45
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-11-20 19:11:48 -0600

    video 325
    Mostrar las imagenes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,21 @@
 export const environment = {
   production: false,
   firebase:  {
-    apiKey: 'AIzaSyCmjRsg49_e60XTELGouDDH4bO42M6sh3Y',
+   /* apiKey: 'AIzaSyCmjRsg49_e60XTELGouDDH4bO42M6sh3Y',
   authDomain: 'fir-eac1c.firebaseapp.com',
   databaseURL: 'https://fir-eac1c.firebaseio.com',
   projectId: 'fir-eac1c',
   storageBucket: 'fir-eac1c.appspot.com',
   messagingSenderId: '261136043940',
-  appId: '1:261136043940:web:15a36d063153231b663b31',
+  appId: '1:261136043940:web:15a36d063153231b663b31',*/
+  apiKey: 'AIzaSyCafk2MCzZz39gjA-Iw0kM-30K1ylyio6U',
+    authDomain: 'fotosfirebase-49f24.firebaseapp.com',
+    databaseURL: 'https://fotosfirebase-49f24.firebaseio.com',
+    projectId: 'fotosfirebase-49f24',
+    storageBucket: 'fotosfirebase-49f24.appspot.com',
+    messagingSenderId: '209666662040',
+    appId: '1:209666662040:web:0dc73d966f19e012db7aed',
+    measurementId: 'G-V2G24DVWH4',
   }
 };
 
